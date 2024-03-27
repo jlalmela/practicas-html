ff16484409420f87a913998d6b11b2ead84a51cc
José Luis Almela <95770467+jlalmela@users.noreply.github.com>
2024-03-27 19:27:22 +0100

    actualizar

--- ejemplo-18/app.js
+++
@@ -1,8 +0,0 @@
-// Importar la biblioteca `qrcode.min.js`
-const QRCode = require("qrcode.min");
-
-// Generar el código QR para el DNI
-const qrcode = new QRCode();
-qrcode.addData("12345678A");
-qrcode.setSize(150);
-qrcode.render("qrcode.png");
